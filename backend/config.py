6fec9dcb15f71e0c2a284eb3f1c8b91bb3a62da5
ShashiBhushanSharma-debug <shashibhushan.mgs3541@gmail.com>
2026-08-23 14:48:40 +0530

    Feat(Backend), Feat(script): Added

---
+++ backend/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    PROJECT_NAME: str = "AegisFlow Fintech AI Engine"
+    API_V1_STR: str = "api/v1"
+
+    SUPABASE_URL:str = os.getenv("SUPABASE_URL","")
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY", "")
+
+settings = Settings()
